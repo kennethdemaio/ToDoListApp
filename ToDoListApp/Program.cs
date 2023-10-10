37f64e7588fe504c7160cbee2c4a8ae149805e60
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-10 16:10:55 -0400

    Early stages of the remove system complete. I will refactor from string literals to index values of the list

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -5,28 +5,47 @@ List<string> ToDoList = new List<string>();
 
 bool RunProgram = true;
 while (RunProgram){
-    Console.WriteLine("\nPlease type 1 to add to your list");
+    Console.WriteLine("\nPlease type 1 to add to your list"); // Complete
     Console.WriteLine("Please type 2 to remove from your list");
-    Console.WriteLine("Please type 3 to view your list");
+    Console.WriteLine("Please type 3 to view your list"); // Complete
     Console.WriteLine("Please type 4 to mark a task as complete");
-    Console.WriteLine("Please type 5 to end program");
+    Console.WriteLine("Please type 5 to end program"); // Complete
     string Response = Console.ReadLine();
         switch (Response){
 
             case "1":
-                Console.WriteLine("Please enter the task you would like to add to your todo list.");
+                Console.WriteLine("\nPlease enter the task you would like to add to your todo list.");
                 string AddToList = Console.ReadLine();
                 if (!string.IsNullOrEmpty(AddToList)){
+
                     ToDoList.Add(AddToList);
+
                 }
                 else{
+
                     Console.WriteLine("Please input a valid response and not null");
+
                 }
                break;
 
             case "2":
-                 Console.WriteLine("this is where i'll print the array of todos");
-                 Console.WriteLine("Please enter the task you would like to be removed from your todo list");
+                foreach (string number in ToDoList)
+                {
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine("\nPlease enter the task you would like to be removed from your todo list");
+                string RemoveFromList = Console.ReadLine();
+                if (ToDoList.Contains(RemoveFromList)){
+
+                    ToDoList.Remove(RemoveFromList);
+                    Console.WriteLine($"{RemoveFromList} was removed from your todo list");
+
+                }
+                else{
+
+                    Console.WriteLine("Value not found");
+
+                }
                 break;
 
             case "3":
@@ -38,14 +57,16 @@ while (RunProgram){
 
             case "4":
                 Console.WriteLine("this is where i'll print the array of todos");
-                Console.WriteLine("Please enter the task you would like to be marked as complete.");
+                Console.WriteLine("\nPlease enter the task you would like to be marked as complete.");
                 break;
 
             case "5":
-                Console.WriteLine("test5");
+                Console.WriteLine("\nHave a nice day!");
+                Environment.Exit(0);
                 break;
+
             default:
-                Console.WriteLine("Invalid input");
+                Console.WriteLine("\nInvalid input");
                 break;
         }
 }
