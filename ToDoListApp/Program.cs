d498ac8b841dde857336ffada6360019afb62088
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-10 02:16:37 -0400

    Added switch statement

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Github test");
+﻿Console.WriteLine("Welcome to my Todo list program!");
+Console.WriteLine("Please select from the options below.");
+
+bool RunProgram = true;
+while (RunProgram){
+
+    Console.WriteLine("\nPlease type 1 to add to your list");
+
+    Console.WriteLine("Please type 2 to remove from your list");
+
+    Console.WriteLine("Please type 3 to view your list");
+
+    Console.WriteLine("Please type 4 to mark a task as complete");
+
+    Console.WriteLine("Please type 5 to end program");
+    
+    string Response = Console.ReadLine();
+    switch (Response){
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+    }
+}
