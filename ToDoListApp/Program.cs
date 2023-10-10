ad521ed05c0ac0646d9b829edf160aa3af89d668
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-10 15:52:57 -0400

    Got the functional of adding to the list complete.

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -3,37 +3,50 @@
 Console.WriteLine("Welcome to my Todo list program!");
 Console.WriteLine("Please select from the options below.");
 
+List<string> ToDoList = new List<string>();
+
 bool RunProgram = true;
 while (RunProgram){
-
     Console.WriteLine("\nPlease type 1 to add to your list");
-
     Console.WriteLine("Please type 2 to remove from your list");
-
     Console.WriteLine("Please type 3 to view your list");
-
     Console.WriteLine("Please type 4 to mark a task as complete");
-
     Console.WriteLine("Please type 5 to end program");
-    
     string Response = Console.ReadLine();
         switch (Response) {
             case "1":
                 Console.WriteLine("Please enter the task you would like to add to your todo list.");
+                string AddToList = Console.ReadLine();
+                if (AddToList != null){
+                    ToDoList.Add(AddToList);
+                }
+                else{
+                    Console.WriteLine("Please write a valid response");
+                }
+                foreach (string number in ToDoList){
+                Console.WriteLine(number);
+                }
                 break;
+
             case "2":
                  Console.WriteLine("this is where i'll print the array of todos");
                  Console.WriteLine("Please enter the task you would like to be removed from your todo list");
                 break;
+
                 case "3":
                 Console.WriteLine("This is where i'll print the list");
                 break;
+
             case "4":
                 Console.WriteLine("this is where i'll print the array of todos");
                 Console.WriteLine("Please enter the task you would like to be marked as complete.");
                 break;
+
             case "5":
                 Console.WriteLine("test5");
+                break;
+            default:
+                Console.WriteLine("Invalid input");
                 break;
         }
 }
