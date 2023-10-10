46982ac41fc355f7e6786bb6aacaf08ea09c4729
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-10 02:27:23 -0400

    Imported collections generic to use lists. WIll be storing the todo strings in a list because arrays are fixed but lists can expand in size dynamicly.

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Welcome to my Todo list program!");
+﻿using System.Collections.Generic;
+
+Console.WriteLine("Welcome to my Todo list program!");
 Console.WriteLine("Please select from the options below.");
 
 bool RunProgram = true;
@@ -15,16 +17,23 @@ while (RunProgram){
     Console.WriteLine("Please type 5 to end program");
     
     string Response = Console.ReadLine();
-    switch (Response){
-        case "1":
-            break;
-        case "2":
-            break;
-        case "3":
-            break;
-        case "4":
-            break;
-        case "5":
-            break;
-    }
+        switch (Response) {
+            case "1":
+                Console.WriteLine("Please enter the task you would like to add to your todo list.");
+                break;
+            case "2":
+                 Console.WriteLine("this is where i'll print the array of todos");
+                 Console.WriteLine("Please enter the task you would like to be removed from your todo list");
+                break;
+                case "3":
+                Console.WriteLine("This is where i'll print the list");
+                break;
+            case "4":
+                Console.WriteLine("this is where i'll print the array of todos");
+                Console.WriteLine("Please enter the task you would like to be marked as complete.");
+                break;
+            case "5":
+                Console.WriteLine("test5");
+                break;
+        }
 }
